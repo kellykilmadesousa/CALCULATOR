e7df7b1d4d51f1e8371ad889be6ae9dba50f9ad5
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2023-11-09 10:03:45 -0300

    :sparkles: funções limpar e voltar dos botões

--- js/index.js
+++ js/index.js
@@ -5,3 +5,17 @@ function inserir(num){
 function limpar(){
     document.querySelector('.tela').innerHTML = "";
 }
+
+function voltar(){
+    let tela = document.querySelector('.tela').innerHTML;
+    document.querySelector('.tela').innerHTML =  tela.substring(0, tela.length -1);
+}
+
+function somar(){
+    let tela = document.querySelector('.tela').innerHTML;
+    if(tela){
+        document.querySelector('.tela').innerHTML = eval(tela);
+    }else{
+        document.querySelector('.tela').innerHTML = "0"
+    }
+}
